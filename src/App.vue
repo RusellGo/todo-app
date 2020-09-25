65e94e6a95d42052fb178a524dc05dc4d0530a46
RusellGo <3115458498@qq.com>
2020-09-25 20:48:19 +0800

    todo-app结构html编写

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <div class="container">
+      <h1>欢迎使用 Q 待办事项！</h1>
+      <div class="input-add">
+        <input type="text" />
+        <button>
+          <i class="plus"></i>
+        </button>
+      </div>
+      <div class="filters">
+        <span class="filter active">全部</span>
+        <span class="filter">已完成</span>
+        <span class="filter">未完成</span>
+      </div>
+      <div class="todo-list">
+        <div class="todo-item">
+          <label for="todo1">
+            <input type="checkbox" id="todo1" />
+            Todo1
+            <span class="check-button"></span>
+          </label>
+        </div>
+        <div class="todo-item">
+          <label for="todo2">
+            <input type="checkbox" id="todo2" />
+            Todo2
+            <span class="check-button"></span>
+          </label>
+        </div>
+        <div class="todo-item">
+          <label for="todo3">
+            <input type="checkbox" id="todo3" />
+            Todo3
+            <span class="check-button"></span>
+          </label>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
