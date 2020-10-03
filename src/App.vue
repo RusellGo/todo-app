6d2a692acf2c567a9401fc559cc6ca530c716074
RusellGo <3115458498@qq.com>
2020-10-03 11:55:17 +0800

    todo-app项目css编写

--- src/App.vue
+++ src/App.vue
@@ -48,12 +48,135 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  border: 0;
+  font-family: Helvetica, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+/* 整个页面 */
+main {
+  width: 100vw;
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  background-color: rgb(203, 210, 240);
+}
+.container {
+  width: 60%;
+  max-width: 400px;
+  box-shadow: 0 0 24px rgba(0, 0, 0, 0.15);
+  border-radius: 24px;
+  padding: 48px 28px;
+  background-color: rgb(245, 246, 252);
+}
+
+/* 标题 */
+h1 {
+  margin: 24px 0;
+  font-size: 28px;
+  color: #414873;
+}
+
+/* 添加框 */
+.input-add {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+.input-add input {
+  width: 100%;
+  padding: 16px 52px 16px 18px;
+  border-radius: 48px;
+  border: none;
+  outline: none;
+  box-shadow: 0 0 24px rgba(0, 0, 0, 0.08);
+  font-size: 16px;
+  color: #626262;
+}
+.input-add button {
+  position: absolute;
+  right: 0;
+  width: 46px;
+  height: 46px;
+  border-radius: 50%;
+  background: -webkit-linear-gradient(#c0a5f3, #7f95f7);
+  /* background: -webkit-linear-gradient(left bottom, red, yellow); */
+  border: none;
+  outline: none;
+  color: white;
+  cursor: pointer;
+}
+.input-add .plus {
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(#fff, #fff), linear-gradient(#fff, #fff);
+  background-size: 50% 2px, 2px 50%;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+/* 过滤项 */
+.filters {
+  display: flex;
+  margin: 24px 2px;
+  color: #c0c2ce;
+  font-size: 14px;
+}
+.filters .filter {
+  margin-right: 14px;
+  transition: 0.8s;
+}
+.filters .filter.active {
+  color: #6b719c;
+  transform: scale(1.2);
+}
+
+/* 待办事项列表 */
+.todo-list {
+  display: grid;
+  row-gap: 14px;
+}
+.todo-item {
+  background: white;
+  padding: 16px;
+  border-radius: 8px;
+  color: #626262;
+}
+.todo-item label {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+.todo-item label span.check-button {
+  position: absolute;
+  top: 0;
+}
+.todo-item label span.check-button::before,
+.todo-item label span.check-button::after {
+  content: "";
+  display: block;
+  position: absolute;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+}
+.todo-item label span.check-button::before {
+  border: 1px solid #b382f9;
+}
+.todo-item label span.check-button::after {
+  transition: 0.4s;
+  background: #b382f9;
+  transform: translate(1px, 1px) scale(0.8);
+  opacity: 0;
+}
+.todo-item input {
+  margin-right: 16px;
+  opacity: 0;
+}
+.todo-item input:checked + span.check-button::after {
+  opacity: 1;
 }
 </style>
